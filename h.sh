ea268c3058f5da3c31d5dc39515830708430735f
abdulbadii <abdulbadii@yandex.com>
2018-09-25 08:24:04 +0700

    Update h.sh

--- h.sh
+++ h.sh
@@ -2,11 +2,12 @@
 h(){
 # Literal to regex conversion
 t=`echo $@ | sed -E 's/[{}\.]/\\\&/g; s/\*/.*/g; s/\?/./; s/\\$/\\\s*$/'`
-s=;IFS=$'\n'
+s=;IFS=$'\n' #for Windows port Msys: \r\n, for Mac: \r
 for a in `history`;{ s="$a\n$s"; }
-a=`echo -e $s |sed -E "s/\s*[0-9]+\s+($t.*)/\1/i; tq; d; :q q"`
 s=`echo -e $s |sed -E "s/\s*([0-9]+)\s+$t.*/\1/i; tq; d; :q"`
-i=;for d in $s
+i=
+#for else than linux set it back here to \n so this line: IFS=$'\n'
+for d in $s
 {
 history -d $d
 echo erasing $d
@@ -14,6 +15,10 @@ let i++
 }
 if test "$i"
 	then	echo "$i erasure(s)"
-	i=${@};test ${#i} -gt 3 &&{ history -s "$a";echo retaining/restoring: "\"$a\""; }
+	i=$@
+	test ${#i} -gt 4 &&{
+	a=`echo -e $s |sed -E "s/\s*[0-9]+\s+($t.*)/\1/i; tq; d; :q q"`
+	history -s "$a";echo retaining/restoring: "\"$a\""
+	}
 fi
 }
