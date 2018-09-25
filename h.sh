9b04d50537ffd5ae08be4e6ebadca7583ff19070
abdulbadii <abdulbadii@yandex.com>
2018-09-25 09:04:17 +0700

    Update h.sh

--- h.sh
+++ h.sh
@@ -1,13 +1,12 @@
 #! /usr/bin/bash
 h(){
-# Literal to regex conversion
 t=`echo $@ | sed -E 's/[{}\.]/\\\&/g; s/\*/.*/g; s/\?/./; s/\\$/\\\s*$/'`
-s=;IFS=$'\n' #for Windows port Msys: \r\n, for Mac: \r
+s=;IFS=$'\n'   #for Windows port Msys: \r\n, for Mac: \r
 for a in `history`;{ s="$a\n$s"; }
-s=`echo -e $s |sed -E "s/\s*([0-9]+)\s+$t.*/\1/i; tq; d; :q"`
+n=`echo -e $s |sed -E "s/\s*([0-9]+)\s+$t.*/\1/i; tq; d; :q"`
 i=
-#for else than linux set it back here to \n so this line: IFS=$'\n'
-for d in $s
+# Else than linux here set it back to \n thus: IFS=$'\n'
+for d in $n
 {
 history -d $d
 echo erasing $d
@@ -21,4 +20,5 @@ if test "$i"
 	history -s "$a";echo retaining/restoring: "\"$a\""
 	}
 fi
+history -w
 }
