0b924cf9be1512e0079bc3248e78beb54bb8f90b
abdulbadii <abdulbadii@yandex.com>
2018-09-28 07:36:12 +0700

    Update h.sh

--- h.sh
+++ h.sh
@@ -1,17 +1,26 @@
 #! /usr/bin/bash
 h(){
 t=`echo $@ | sed -E 's/[{}\.$]/\\\&/g; s/\*/.*/g; s/\?/./g'`
+u=${#t}
 [[ $t =~ \.$ ]] &&t="$t?$"
 s=;IFS=$'\n'  # change to \r\n for Windows port, \r for Mac port
+if [[ `echo $-` =~ x ]] ;then set +x;for a in `history`;{ s="$a\n$s"; };n=`echo -e $s |sed -E "s'^\s*([0-9]+)\s+$t.*'\1'i; tq; d; :q"`;set -x
+else
 for a in `history`;{ s="$a\n$s"; }
-n=`echo -e $s |sed -E "s/^\s*([0-9]+)\s+$t.*/\1/i; tq; d; :q"`
-i=0;    # change back IFS=$'\n' for else than Linux
+n=`echo -e $s |sed -E "s'^\s*([0-9]+)\s+$t.*'\1'i; tq; d; :q"`
+fi
+i=0;
+# else than Linux add a line to set: IFS=$'\n'
 for d in $n
 {
-test $i -lt 1 &&echo retaining the $d th history ||history -d $d
+if test $i -lt 1 -a $u -gt 5 ;then
+	echo retaining the $d th history
+else
+	history -d $d
+fi
 let i++
 }
-test "$i" -gt 1 &&echo "$i erasures"
-history -w;echo last 21:;history 21
+echo "$i erasure(s)"
+history -w;echo last 21 lines:;history 21
 unset IFS # set to default
 }
