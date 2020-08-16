8f8ed7d8edf9d9b001e100932f4ba47f5b8c16cd
abdulbadii <abdulbadii@yandex.com>
2020-08-16 17:45:18 +0700

    add

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-Tidying up Bash commands history by having good control in erasing certain line(s) by specific number(s) (range) or by a string segment being within a history line string.
+Click, copy "hist.sh" above, then paste, prepend the Bash functions inside ~/.bashrc file   
+
+Tidying up Bash commands history by having good control in erasing certain line(s) by specific number(s) / range) or by a string segment in a history line.   
+
+simply type h then it'll show first 17 lines of last shell commands e.g:
+
+$ h   
+  366  pushd --help
+  367  pushd 
+  368  pushd -n debian/
+  369  pushd  build/
+  370  debuild -i -uc -us -b
+  371  ls debuild*
+  372  ls -hs
+  373  cd ../debian/
+  357  sudo rm 60\ Ebooks\ That\ Will\ Change\ Your\ Life.zip
+  358  cd
+  359  cp n.sh .config/autostart-scripts/autostart.sh
+  360  rm .config/autostart-scripts/autostart.sh
+  361  cp .bash_profile /usr/bin/autostart.sh
+  362  cd roxterm/
+  363  cd debian/
+  364  cat -n install 
+  365  pushd
+Show the next 9? (Enter/Spc: from newer , b: from older, 0..9 delete by number, else quit) 
+
+If one hit enter, it'll show downwardly continuation of line 366, line 357 - 365    
+or if hit b, it'll show history line 1 - 8 and the next b press the continuation of line 9, line 10 - 17   
+or if put in 371-359, it will remove these lines
+or if put some strings, it'll remove any history line having the string
--- hist.sh
+++ hist.sh
@@ -1,50 +1,56 @@
 h(){
-if [ -z "$1" ] ;then history  -d -1
+if [ -z "$1" ] ;then history -d -1
 	b=1
-	history 17;n=17
+	history 13;n=13
 	while : ;do
-	read -n 1 -p 'Show the next 9 more (Enter/Spacebar : from newer list, b : from beginning, 0..9 delete the number history, other is quit)? ' m
-	if [[ $m == [0-9]* ]] ;then
-		read n
-		eval set -- $m$n
-		break
-	elif [ "$m" == "b" ] ;then
+		read -n 1 -p 'Show the next 9? (Enter/Spc: from newer , Ctrl-o: from older, 0..9 delete by number, Others to input string, Escape/Ctrl-c quit) ' m
+		case $m in
+		[0-9]*|-[1-9]*|[\!-~A-z]*)
+			read n
+			eval set -- $m$n
+			break;;
+		*)	echo
+			if [[ `echo $m|sed -n l` == '\033$' ]] ;then			# ESC
+				return
+				
+			elif [[ `echo $m|sed -n l` == '\017$' ]] ;then			# Ctrl o
+				history | tail -n+$b| head -n13
+				((b+=13))
+			else
+				((n+=13))
+				history $n| head -n13
+			fi;;
+		esac
 		echo
-		history | tail -n+$b| head -n9
-		((b+=9))
-	elif [ -z "$m" ] ;then
-		((n+=9))
-		history $n| head -n9
-	else break;
-	fi
 	done
 fi
 for a
 {
-case ${a} in
---help|-[acdnprsw])
+if [[ $a =~ [1-9]+ ]] ;then
+	history -d $a
+elif [[ $a =~ [0-9]+-([1-9]+)?|-[1-9]+ ]] ;then
+	l=${a%-*}
+	u=${a#*-}
+	[ $u ] ||{			# If no UPPER BOUNDARY
+		[ $l ] ||{ history -c;return; } #If no argument but -, it means clean all
+		t=`history 1`
+		u=${t#*[0-9] }
+		u=${t:0: -${#u}}
+	}
+	((l)) || l=1
+	let i=u-l
+	((i<0)) &&let i=-i
+	((++i))
+	while((i--)) ;do
+		history -d $l
+	done
+elif [[ ${a} =~ --help|-[acdnprsw] ]] ;then
 	history  -d -1
-	history $@;;
--[1-9]*|[0-9]*-*)
-		l=${a%-*}
-		u=${a#*-}
-		[ $u ] ||{			# If no UPPER BOUNDARY
-			((l))||{ history -c;return; } #If no argument but - means clean all
-			t=`history 1`
-			u=${t#*[0-9] }
-			u=${t:0: -${#u}}
-		}
-		((l)) || l=1
-		let i=u-l
-		((i<0)) &&let i=-i
-		((++i))
-		while((i--));do
-			history -d $l
-		done;;
-[1-9]*)
-	history -d $a;;
-*) i=;for e in `history|sed -nE "s/^\s*([0-9]+)\s+.*$a.*/\1/i p"`
-	{ let e-=i++;history -d $e; };;
-esac
+	history $@
+else
+	i=;for e in `history|sed -nE "s/^\s*([0-9]+)\s+.*$a.*/\1/i p"`
+	{ let e-=i++;history -d $e; }
+fi
 }
+echo
 }
