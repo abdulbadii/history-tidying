e0e53c190cadf61010032064d15448588c98c23f
abdulbadii <abdulbadii@yandex.com>
2020-08-30 05:44:07 +0700

    improve

--- hist.sh
+++ hist.sh
@@ -1,41 +1,53 @@
 h(){
 if [ -z "$1" ] ;then history -d -1
-	b=-12;f=
+	b=-12
 	history 13;l=13
+	IFS=''
 	while : ;do
-		read -n 1 -p 'Show the next 13? (Enter/Spc: from newer , Ctrl-o: from older, Escape/Ctrl-C quit, 0..9 delete by number, Others is as a deletion substring) ' m
+		 echo -e '\033[44;1;37m'
+		 read -d '' -sn 1 -p 'Show the next 13? (Enter: no/quit, Space: from newer, Ctrl-b: from begin, [-]0..9[-] delete by number/range, Others as a deletion substring) ' m
+		echo -ne '\033[0m'
 		case $m in
-		[0-9]*|-[1-9]*|[\!-~A-z]*)
+		$'\x0a')
+			unset IFS;return;;
+		$'\x20')
+			((l+=13))
+			history $l| head -n13
+		;;
+		$'\x02') # Ctrl b
+			echo
+			((b+=13))
+			history | tail -n+$b| head -n13
+		;;
+		[0-9]|-)
+			echo -n $m
+			read n
+			eval set -- $m$n
+			break;;
+		*)	echo -n $m
 			read n
 			s=$m$n
 			s=${s//(/\\(}
-			eval set -- ${s//)/\\)}
+			eval set -- \'${s//)/\\)}\'
 			break;;
-		*)	echo
-			if [[ `echo $m|sed -n l` == '\033$' ]] ;then			# ESC
-				return
-			elif [[ `echo $m|sed -n l` == '\017$' ]] ;then		# Ctrl o
-				echo
-				((b+=13))
-				history | tail -n+$b| head -n13
-				f=1
-			else
-				((l+=13))
-				history $l| head -n13
-				f=
-			fi;;
 		esac
 	done
 fi
+unset IFS
+
 for a
 {
-if [[ $a =~ [1-9]+ ]] ;then
-	history -d $a
-elif [[ $a =~ [0-9]+-([1-9]+)?|-[1-9]+ ]] ;then
-	l=${a%-*}
-	u=${a#*-}
+if [[ $a =~ --help|-[acnprsw] ]] ;then
+	history  -d -1
+	history $@
+	break
+elif [[ $a =~ ^[1-9]+$ ]] ;then
+		history -d $a
+		l=$a
+elif [[ $a =~ ^([0-9]+)?(-[0-9]+)? ]] ;then
+	l=${BASH_REMATCH[1]}
+	u=${BASH_REMATCH[2]}
 	[ $u ] ||{			# If no UPPER BOUNDARY
-		[ $l ] ||{ history -c;return; } #If no argument but -, it means clean all
 		t=`history 1`
 		u=${t#*[0-9] }
 		u=${t:0: -${#u}}
@@ -47,24 +59,19 @@ elif [[ $a =~ [0-9]+-([1-9]+)?|-[1-9]+ ]] ;then
 	while((i--)) ;do
 		history -d $l
 	done
-elif [[ ${a} =~ --help|-[acdnprsw] ]] ;then
-	history  -d -1
-	history $@
 else
 	i=
 	if [ ${#a} -gt 2 ] ;then
-        for e in `history|sed -nE "s/^\s*([0-9]+)\s+.*$a.*/\1/i p"`
-        { let e-=i++;history -d $e; }
-    else
-        for e in `history|sed -nE "s/^\s*([0-9]+)\s+$a\$/\1/i p"`
-        { let e-=i++;history -d $e; }
-    fi
-	if [ $f ] ;then
-		history | tail -n+$b| head -n13
+		for l in `history|sed -nE "s/^\s*([0-9]+)\s+.*$a.*/\1/ p"`
+			{ let l-=i++;history -d $l; }
 	else
-		history $l| head -n13
+		for l in `history|sed -nE "s/^\s*([0-9]+)\s+$a\$/\1/ p"`
+			{ let l-=i++;history -d $l; }
 	fi
 fi
+let l-=5+`history |head -n1| sed -E 's/^\s*([0-9]+).*/\1/ p'`
+[ $l -lt 0 ] &&l=0
+history |tail -n+$l | head -n13
 }
 echo
 }
