43c118e93b54daf1855a1b147164f08868fb1e24
abdulbadii <abdulbadii@yandex.com>
2020-09-01 23:21:15 +0700

    fix

--- hist.sh
+++ hist.sh
@@ -4,9 +4,10 @@ while : ;do
 t=`history|head -n1`
 of=${t#*[0-9] }
 of=${t:0: -${#of}}
+t=
+IFS=$'\n'
+for ll in `history` ;{ ((t++)); }
 
-unset t r
-IFS=$'\n';for ll in `history` ;{ ((t++)); }
 if [ -z "$1" ] ;then
 	((F)) &&{	history -d -1;history 17;F=;	}
 	b=1
@@ -17,7 +18,7 @@ if [ -z "$1" ] ;then
 		 read -d '' -sn 1 -p 'Show the next 17? (Enter: no/quit, Space: from newer, Ctrl-b: from begin, [-]0..9[-] delete by number/range, Others as a deletion substring) ' m
 		echo -ne '\033[0m'
 		case $m in
-		$'\x20')
+		$'\x20') #SPC
 			echo
 			if [ $((l+=17)) -gt $t ] ;then
 				let l-=t-1
@@ -26,8 +27,7 @@ if [ -z "$1" ] ;then
 			else
 				history $l| head -n17
 			fi;;
-		[0-9]|-)
-			echo -n $m
+		[0-9]|-)	echo -n $m
 			read n
 			eval set -- $m$n
 			break;;
@@ -39,8 +39,8 @@ if [ -z "$1" ] ;then
 				history	|head -n$b
 				let ++b
 			fi;;
-		$'\x0a')
-			unset IFS;echo;return;;
+		$'\x0a'|$'\x1b')
+			break 2;;
 		*)	echo -n $m
 			read n
 			s=$m$n
@@ -50,7 +50,8 @@ if [ -z "$1" ] ;then
 		esac
 	done
 fi
-unset IFS j s l u ; B=0
+
+unset IFS j s l b u; B=0
 for a
 {
 if [[ $a =~ --help|-[acnprsw] ]] ;then
@@ -70,6 +71,8 @@ elif [[ $a =~ ^([1-9][0-9]*)-([1-9][0-9]*)?$ ]] || [[ $a =~ ^()-([1-9][0-9]*)$ ]
 	[ $B -lt $u ] && let u-=i+j
 	while((i--)) ;do history -d $l
 	done
+elif [ "$a" = - ] ;then
+	history -d -1;break 2
 else
 	i=
 	[ ${#a} -gt 2 ] && a=.\*$a.\*
@@ -77,19 +80,26 @@ else
 		{ let l-=i++;history -d $l; }
 	break
 fi
-B=$u
 ((++j))
+b=$B
+B=$u
 }
 
-((s=B? u-B: 17))
+((s=b? u-b: 17))
 s=${s#-}
-((bo=u>B? B-4-of: u-4-of))
-((bo=bo<0? 0: bo))
-((s=s>11? s+3: 15))
-echo;history |tail -n+$bo |head -n$s
-((F)) &&{ unset IFS;return; }
-eval set --
+((bo=u>b? b-4-of: u-4-of))
+((bo<0))&&bo=0
+history |tail -n+$bo |head -n$((s=s>13? s+3: 17))
+((F)) &&break
+set --
 done
-for l in `history|sed -nE "s/^\s*([0-9]+)\*?\s*\$/\1/ p"`
-	{ let l-=i++;history -d $l; }
+IFS=$'\n'
+i=;for l in `history`
+{
+	[[ $l =~ ^[\ \\t]*([0-9]+)\*?[\ \\t]*$ ]] &&{
+		let l=BASH_REMATCH[1]-i++
+		history -d $l
+	}
+}
+unset IFS;echo
 }
