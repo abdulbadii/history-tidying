c072b9ebc0370c9740a8f7ceb9c14902d6a5a21a
abdulbadii <abdulbadii@yandex.com>
2020-09-07 19:55:47 +0700

    fix

--- hist.sh
+++ hist.sh
@@ -1,38 +1,39 @@
 h(){
 history -d -1
+[[ $1 =~ --help|-[acnprsw] ]] &&{	history $@;return;}
 F=1
+	[[ `history|head -n1` =~ ^\ *([0-9]+) ]]
+	t='\!'
+	let t=${t@P}-${BASH_REMATCH[1]}+1
+	l=17;h=$t
 while : ;do
 if [ -z "$1" ] ;then
 	((F))&&{	history 17;F=;	 }
-	b=1
-	l=17
 	IFS='';while : ;do
-		[[ `history|head -n1` =~ ^\ *([0-9]+) ]]
-		o=${BASH_REMATCH[1]}
-		t='\!';printf -vT ${t@P}
-		let t=T-o+1
 		read -sN 1 -p "`echo $'\e[44;1;37m'`Show the next 17? (Enter: abort, Up: from end/newer, Down: from begin, [-]n[-][n] erase by number n or range n-n, Others as deletion substring) `echo -e '\e[0m\n\r'`" m
 		case $m in
 		$'\x1b') #ESC
-			read -N 2 n
-			echo
-			case $n in
-			[A) #UP
-				if((((l+=17))>t)) ;then
-					let l-=t
-					history |head -n$((17-l))
-					history $l
-				else
-					history $l| head -n17
-				fi;;
-			[B) #DN
-				history | tail -n+$b	| head -n17
-				if [ $((b+=17)) -gt $t ]; then
-					let b-=t
-					history	|head -n$b
-					let ++b
-				fi;;
-			esac;;
+			read -N 1 m
+			if [[ $m != [ ]] ;then return
+			else
+				read -N 1 m
+				echo
+				case $m in
+				A) #UP
+					if((((l+=17))>t)) ;then
+						let l-=t
+						history |head -n$((17-l))
+						history $l
+					else
+						history $l| head -n17
+					fi;;
+				B) #DN
+					history $h	| head -n17
+					((((h-=17))<0)) &&{
+						history	$t| head -n$((-h))
+						let h+=t;	};;
+				esac
+			fi;;
 		$'\x0a')
 			break 2;;
 		*)
@@ -41,88 +42,85 @@ if [ -z "$1" ] ;then
 				eval set -- $m
 				break
 			else
-				s=${m//(/\\(}
-				eval set -- \'${s//)/\\)}\'
+				eval set -- \'$m\'
 				break
 			fi;;
 		esac
 	done
 fi
-unset IFS i j k s l b u B
+unset IFS k s l b u B i j D
 for a
 {
-[[ `history|head -n1` =~ ^\ *([0-9]+) ]]
-o=${BASH_REMATCH[1]}
-t='\!';printf -vT ${t@P}
-let t=T-o+1
-if((F)) &&[[ $a =~ --help|-[acnprsw] ]] ;then
-	history  -d -1
-	history $@
-	unset IFS;return
-elif [[ $a =~ ^[1-9][0-9]*$ ]] ;then
-		u=$a
+	if [[ $a =~ ^[1-9][0-9]*$ ]] ;then
+			u=$a
+			let D=j+k
+			((B<u)) && let u-=D
+			history -d $u 2>/dev/null &&((++j))
+			((b=B? B: u))
+	elif [[ $a =~ ^([1-9][0-9]*)-([1-9][0-9]*)?$ ]] || [[ $a =~ ^()-([1-9][0-9]*)$ ]] ;then
+		l=${BASH_REMATCH[1]}
+		u=${BASH_REMATCH[2]}
+		((l=l? l: 1))
+		((u=u? u: T))
+		((u<l)) &&{		m=$u;u=$l;l=$m; }
 		let D=j+k
 		((B<u)) && let u-=D
-		history -d $u 2>/dev/null &&((++j))
-		((b=B? B: u))
-elif [[ $a =~ ^([1-9][0-9]*)-([1-9][0-9]*)?$ ]] || [[ $a =~ ^()-([1-9][0-9]*)$ ]] ;then
-	l=${BASH_REMATCH[1]}
-	u=${BASH_REMATCH[2]}
-	((l=l? l: 1))
-	((u=u? u: T))
-	((u<l)) &&{		m=$u;u=$l;l=$m; }
-	let D=j+k
-	((B<u)) && let u-=D
-	let i=u-l+1
-	while((i--)) ;do history -d $l 2>/dev/null &&((++k))
-	done
-	b=$l
-elif ((F)) && [ "$a" = - ] ;then break 2
-else
-	a=${a//\\/\\\\};a=${a//\//\\/};a=${a//'/\\'};a=${a//"/\\"};a=${a//./\\.};a=${a//\*/\\*}
-	a=${a//\?/\\?};a=${a//\[/\\[};a=${a//\]/\\]};a=${a//\(/\\(};a=${a//\)/\\)};a=${a//\{/\\{}
-	a=${a//\}/\\\}}
-	((${#a}>1)) &&{
-		if [[ $a =~ ^[[:graph:]].*[[:graph:]]$ ]] ;then
-			a=.\*$a.\*
-		elif [[ $a =~ ^\ +.*[[:graph:]]+$ ]] ;then
-			a=$a.\*
-		elif [[ $a =~ ^[[:graph:]].*\ +$ ]] ;then
-			a=.\*$a
-		fi
-	}
-	i=;IFS=$'\n'
-	for u in `history|sed -nE "s/^\s*([0-9]+)\s+$a\$/\1/p"`
-	{
-		((i)) ||{
-			let m=T-u+1
-			echo -e Line: '\033[41;1;37m'`history $m|head -n1`'\033[0m'
-			b=$u
-		}
-		history -d $((u-i++))
-	}
-	if ! ((i)) ;then echo No history line matched. Did nothing;set --;continue 2
-	elif ((i>1)) ;then echo -e and else after this, up to $i lines have been erased
-	else echo has been erased
-	fi
-fi
-B=$u
-}
-((u<b))&&{	m=$b;b=$u;u=$m; }
-((b=b>3? b-3: 1))
-if((u-b<17)) ;then
-	if((T-b<17)) ;then
-		history $((l=17));let l+=17
+		let i=u-l+1
+		while((i--)) ;do history -d $l 2>/dev/null &&((++k))
+		done
+		b=$l
 	else
-		history |tail -n+$((l=b)) |head -n17
-		let l+=17
+		a=${a//\\/\\};a=${a//#/\#}
+		a=${a//'/\\'};a=${a//"/\\"};a=${a//./\\.};a=${a//\*/\\*}
+		a=${a//\?/\\?};a=${a//\[/\\[};a=${a//\]/\\]};a=${a//\(/\\(};a=${a//\)/\\)};a=${a//\{/\{}
+		a=${a//\}/\}}
+		((${#a}>1)) &&{
+			if [[ $a =~ ^[[:graph:]].*[[:graph:]]$ ]] ;then
+				a=.\*$a.\*
+			elif [[ $a =~ ^\ +.*[[:graph:]]+$ ]] ;then
+				a=$a.\*
+			elif [[ $a =~ ^[[:graph:]].*\ +$ ]] ;then
+				a=.\*$a
+			fi
+		}
+		D=;IFS=$'\n'
+		for u in `history|sed -nE "s#^\s*([0-9]+)\s+$a\\$#\1#p"`
+		{
+			((D)) ||{
+				let m=T-u+1
+				echo -e Line: '\033[41;1;37m'`history $m|head -n1`'\033[0m'
+				b=$u
+			}
+			history -d $((u-D++))
+		}
+		if ! ((D)) ;then echo No history line matched. Did nothing;set --;continue 2
+		elif ((D>1)) ;then echo -e and else after this, up to $D lines have been erased
+		else echo has been erased
+		fi
+	fi
+	B=$u
+	[[ `history|head -n1` =~ ^\ *([0-9]+) ]]
+	o=${BASH_REMATCH[1]}
+	t='\!';printf -vT ${t@P}
+	let t=T-o+1
+}
+b=$b
+u=$u
+((u<b))&&{	m=$b;b=$u;u=$m; }
+((b=b>4? b-4: 1))
+if((u-D-b+3<17)) ;then
+	if((T-b+3<17)) ;then
+		history $((l=17))
+	else
+		let l=T-b+D+4
+		history $l |head -n17
 	fi
 else
-	history |tail -n+$b |head -n7
+	history $((T-b)) |head -n7
 	echo '  '...
-	history $((l=T-u+D-4)) |head -n7
-	let l+=17
+	history $((l=T-u+D+4)) |head -n7
 fi
+let l+=17
 ((F))&&break
 set --
 done
