3e06be2ac1a661836050b0d4b113455d6a1130e5
abdulbadii <abdulbadii@yandex.com>
2022-07-24 18:40:21 +0700

    fix efficiency

--- hist.sh
+++ hist.sh
@@ -2,8 +2,7 @@ h(){ #BEGIN h
 [[ $1 =~ ^-cr|^-rc ]] &&{ history -c;history -r;return;}
 [[ $1 =~ ^--help$|^-[anprsw]$|^\. ]] &&{ history ${@#.};return;}
 [[ `history|head -1` =~ ^[[:space:]]*([0-9]+) ]]
-let off=BASH_REMATCH[1]-1
-let T=HISTCMD-off
+let B=HISTCMD-BASH_REMATCH[1]+1
 C=;D=;F=1;L=25;U=0
 while : ;do
 if [ -z "$1" ] ;then
@@ -19,8 +18,8 @@ if [ -z "$1" ] ;then
 				case $m in
 				A) #UP
      U=$L
-					if((((L+=25))>T)) ;then
-						let L-=T
+					if((((L+=25))>B)) ;then
+						let L-=B
 						history |head -$((25-L))
 						history $L
 					else
@@ -31,7 +30,7 @@ if [ -z "$1" ] ;then
 					history $U	| head -25
 					((((U-=25))<0)) &&{
 						history| head -$((-U))
-						let U+=T
+						let U+=B
 					}
 				esac
 			fi;;
@@ -46,18 +45,15 @@ if [ -z "$1" ] ;then
 	done
 fi
 unset IFS s b i j k l u C D
-[[ `history|head -1` =~ ^[[:space:]]*([0-9]+) ]];let off=BASH_REMATCH[1]-1
 for a in "$@"
 {
- let B=HISTCMD-off
  if [[ $a =~ ^(-)?(-[1-9][0-9]*)(-[0-9])?$ ]] ;then
   let b=BASH_REMATCH[2]-U
   if [ "${BASH_REMATCH[1]}" ] ;then
    for i in `eval echo {$b..$((BASH_REMATCH[3]-1-U))}` ;{ history -d $i 2>/dev/null &&((++k));}
-   let b+=b+B
 		else
    history -d $b
-   let b+=B
+   #let b+=B
   fi
 	elif [[ $a =~ ^[1-9][0-9]*$ ]] ;then
 		l=$a
@@ -74,14 +70,15 @@ for a in "$@"
   let D=j+k
   ((B<u)) && let u-=D
   ((C=u-l+1))
-  while((C--)) ;do history -d $l 2>/dev/null &&((++k))
-  done
+  while((C--)) ;do history -d $l 2>/dev/null &&((++k));done
   b=$l
 	else
-  a=${a//\//\\/};a=${a//\"/\\\"};a=${a//./\\.};a=${a//\*/\\*}
-		a=${a//\?/\\?};a=${a//\[/\\[};a=${a//\]/\\]}
-  a=${a//\|/\\|};a=${a//\^/\\^};a=${a//\(/\\(};a=${a//\)/\\)}
-  a=${a//\{/\\{};a=${a//\}/\\'}'};a=${a//\$/\\\$}
+  a=${a//\//\\/};a=${a//\"/\\\"};a=${a//./\\.}
+  a=${a//\*/\\*};a=${a//\+/\\+};a=${a//\|/\\|};a=${a//\^/\\^}
+		a=${a//\?/\\?};
+  a=${a//\[/\\[};a=${a//\(/\\(};a=${a//\{/\\{}
+  a=${a//\]/\\]};a=${a//\)/\\)};a=${a//\}/\\'}'}
+  a=${a//\$/\\\$};a=${a//\.../.\*}
 		if ((${#a}>2)) ;then
 			if [[ $a =~ ^[[:graph:]].*[[:graph:]]$ ]] ;then a=.*$a.*
 			elif [[ $a =~ ^[[:space:]]+(.*[[:graph:]])$ ]] ;then a=${BASH_REMATCH[1]}.*
@@ -93,34 +90,35 @@ for a in "$@"
 		for u in `history|sed -nE "s/^\s+([0-9]+)\*?\s+$a$/\1/p"`
 		{
 			((D))||{
-				echo -e Line:'\e[41;1;37m'`history $((B-u))|head -1`'\e[40;1;32m'
+				echo -e Line:'\e[41;1;37m'`history $((B-u+1))|head -1`'\e[40;1;32m'
 				b=$u
 			}
 			history -d $((u-D++))
 		}
 		! ((D)) &&{ echo No history line match. Did nothing;set -;return;}
-  ((D>1)) &&{ echo -ne "and $D following lines ";}
+  ((D>1)) &&{ echo -ne "and others later, total to $D lines ";}
 		echo -e has been erased'\e[m'
 	fi
 	B=$u
+ [[ `history|head -1` =~ ^[[:space:]]*([0-9]+) ]]
+ let B=HISTCMD-BASH_REMATCH[1]+1
 }
-[[ `history|head -1` =~ ^\ *([0-9]+) ]]
-let T=HISTCMD-${BASH_REMATCH[1]}
+let C=B-b
 ((u<b))&&{	m=$b;b=$u;u=$m; }
 ((b=b>4? b-4: 1))
 if((u-D-b+3<25)) ;then
-	if((HISTCMD-b+3<25)) ;then
+	if((C+3<25)) ;then
 		history $((l=25))
 	else
-		let l=HISTCMD-b+D+3
+		let l=C+D+3
 		history $l |head -n25
 	fi
 	let h=l-25
 else
-	history $((HISTCMD-b)) |head -15
+	history $C |head -15
 	echo '  '...
-	if((((l=HISTCMD-u+D+3))<15)) ;then
-		history 15;let h=T
+	if((((l=B-u+D+3))<15)) ;then
+		history 15;let h=B
 	else
 		history $l	|head -15
 		let h=l-15
