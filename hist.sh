1193da93723b06ea627f3c7fe1da515a2b88e62d
abdulbadii <abdulbadii@yandex.com>
2022-07-26 20:14:41 +0700

    fix

--- hist.sh
+++ hist.sh
@@ -34,86 +34,92 @@ if [ -z "$1" ] ;then
 					}
 				esac
 			fi;;
-		$'\x0a')	break 2;;
+		$'\xa')	break 2;;
 		*)
 			read -rei "$m" m
-			[[ ! $m =~ ^[1-9][0-9]*-?([1-9][0-9]*)?$|^--?[1-9][0-9]*(-[0-9])?$ ]] &&{
-    m=${m//\\/\\\\};m=${m//\"/\\\"};m=\"${m//\$/\\\$}\";}
-   eval set -- $m
 			break
 		esac
 	done
 fi
 unset IFS s b i j k l u C D W ln
-for a in "$@"
-{
- if [[ $a =~ ^-([1-9][0-9]*)?$|^--([1-9][0-9]*)-?([1-9][0-9]*)?$ ]] ;then
-  let e=${BASH_REMATCH[3]:-1}+1
-  ! ((d=${BASH_REMATCH[2]})) &&{
-    ((e=d=${BASH_REMATCH[1]:-1}))
-  }
-  ((e>d)) || ((d>25)) || ((((d+=U))>B)) &&{ echo $d or $e is invalid number range;set --;continue 2;}
-  for i in `eval echo {$d..$((e+U))}` ;{
-   history -d -$i 2>/dev/null
-  }
-  if ((d<14)) ;then history 25; else history $((d+12)) |head -25 ;fi
-  did=1;set --;continue 2
-	elif [[ $a =~ ^[1-9][0-9]*$ ]] ;then
-		u=$a
-		let D=j+k
-		((u>HISTCMD)) &&{
-   !((D)) &&{ echo $u is invalid number range;continue 2;}
-   let u-=D
-  }
-		history -d $l 2>/dev/null &&((++j))
-		did=1;((b=B? B: u))
-	elif [[ $a =~ ^([0-9]+)-([1-9][0-9]*)?$ ]] ;then
-		l=${BASH_REMATCH[1]}
-  ((l=l?l:1))
-		u=${BASH_REMATCH[2]}
-  ((u=u?u:HISTCMD))
-  ((u<l)) &&{		m=$u;u=$l;l=$m; }
-  ((u>HISTCMD)) || ((l==u)) &&{ echo $l and/or $u is invalid number range;continue 2;}
-  let D=j+k
-  ((u>B)) &&{
-   let u-=D
-   !((D)) || ((u<B)) &&{ echo $u is invalid number range;continue 2;}
-  }
-  let did=C=u-l+1
-  while((C--)) ;do history -d $l 2>/dev/null &&((++k));done
-  b=$l
-	else
-	 ((W=${#a}>2)); o=$a
-  a=${a//\//\\/};a=${a//\*/\\*};a=${a//\\.\\.\\./.*}
-  a=${a//\+/\\+};a=${a//\|/\\|};a=${a//\^/\\^};a=${a//\?/\\?};
-  a=${a//\[/\\[};a=${a//\(/\\(};a=${a//\{/\\{}
-  a=${a//\]/\\]};a=${a//\)/\\)};a=${a//\}/\\'}'}
-  a=${a//\$/\\\$}
-		if((W)) ;then
-			if [[ $a =~ ^[[:graph:]].*[[:graph:]]$ ]] ;then a=.*$a.*
-			elif [[ $a =~ ^[[:space:]]+(.*[[:graph:]])$ ]] ;then a=${BASH_REMATCH[1]}.*
-			elif [[ $a =~ ^([[:graph:]].*)[[:space:]]+$ ]] ;then	a=.*${BASH_REMATCH[1]}
-			elif [[ $a =~ ^[[:space:]](.*)[[:space:]]+$ ]] ;then	a=${BASH_REMATCH[1]};fi
-		elif [[ $a =~ ^[[:space:]](.*)[[:space:]]+$ ]] ;then	a=.*${BASH_REMATCH[1]}.*
+m=${m//\\/\\\\};m=${m//\"/\\\"};m=\"${m//\$/\\\$}\"
+eval set -- $m
+if [[ $1 =~ ^[0-9]+-?[0-9]*([[:space:]]+.+)?$ ]] ;then
+ for a ;{
+  [[ $a =~ [^-0-9] ]] &&{
+   echo ignoring this non pure numeric range; continue;}
+  if [[ $a =~ ^[0-9]+$ ]] ;then
+   ((u=a?a:1))
+   let D=j+k
+   ((u>HISTCMD)) &&{
+    !((D)) &&{ echo $u is invalid number range;continue 2;}
+    let u-=D
+   }
+   history -d $l 2>/dev/null &&((++j))
+   did=1;((b=B? B: u))
+  else [[ $a =~ ^([0-9]+)-([1-9][0-9]*)?$ ]]
+   l=${BASH_REMATCH[1]}
+   u=${BASH_REMATCH[2]}
+   ((l=l?l:1))
+   ((u=u?u:HISTCMD))
+   ((u<l)) &&{		m=$u;u=$l;l=$m; }
+   ((u>HISTCMD)) || ((l==u)) &&{ echo $l and/or $u is invalid number range;continue 2;}
+   let D=j+k
+   ((u>B)) &&{
+    let u-=D
+    !((D)) || ((u<B)) &&{ echo $u is invalid number range;continue 2;}
+   }
+   let did=C=u-l+1
+   while((C--)) ;do history -d $l 2>/dev/null &&((++k));done
+   b=$l
   fi
-		D=;IFS=$'\n'
-		for u in `history|sed -nE "s/^\s+([0-9]+)\*?\s+$a$/\1/p"`
-		{
-			echo -e '\e[41;1;37m'`history $((B-u+1))|head -1`'\e[m'
-			((D)) || b=$u
-   let ln[D++]=u
-		}
-		! ((D)) &&{ echo -e "\e[41;1;37m$o\e[m wasn't found, did nothing";set --;continue 2;}
-  unset IFS;echo -en '\e[40;1;32m'
-  read -N1 -p "Delete $D line(s) above from command history? (Enter: yes Else: no) " h
-  [[ $h = $'\xa' ]] ||{ echo;set --;continue 2;}
-  for i	in `eval echo {$((D-1))..0}` ;{ history -d ${ln[i]} 2>/dev/null;}
-  echo -e "Finished $D line(s) deletion\e[m";did=1
-	fi
-	B=$u
- [[ `history|head -1` =~ ^[[:space:]]*([0-9]+) ]]
- let B=HISTCMD-BASH_REMATCH[1]+1
-}
+ }
+elif [[ $1 =~ ^-([1-9][0-9]?)?$|^--([1-9][0-9]?)(-([1-9][0-9]?)?)?$ ]] ;then
+ ((e=BASH_REMATCH[3] ? ${BASH_REMATCH[4]:-1}+1 :1))
+ ! ((d=${BASH_REMATCH[2]})) &&{
+   ((e=d=${BASH_REMATCH[1]:-1}))
+ }
+ ((e>d)) || ((d>25)) || ((((d+=U))>B)) &&{ echo $d or $e is invalid number range;set --;continue 2;}
+ for i in `eval echo {$d..$((e+U))}` ;{
+  history -d -$i 2>/dev/null
+ }
+ if ((d<14)) ;then history 25; else history $((d+12)) |head -25 ;fi
+ did=1;set --;continue 2
+else
+ ((W=${#1}>2));
+ a=${1//\//\\/};a=${a//\*/\\*};a=${a//.../.*}
+ a=${a//\+/\\+};a=${a//\|/\\|};a=${a//\^/\\^};a=${a//\?/\\?};
+ a=${a//\[/\\[};a=${a//\(/\\(};a=${a//\{/\\{}
+ a=${a//\]/\\]};a=${a//\)/\\)};a=${a//\}/\\'}'}
+ a=${a//\$/\\\$}
+ if((W)) ;then
+  if [[ $a =~ ^[[:graph:]].*[[:graph:]]$ ]] ;then a=.*$a.*
+  elif [[ $a =~ ^[[:space:]]+(.*[[:graph:]])$ ]] ;then a=${BASH_REMATCH[1]}.*
+  elif [[ $a =~ ^([[:graph:]].*)[[:space:]]+$ ]] ;then	a=.*${BASH_REMATCH[1]}
+  elif [[ $a =~ ^[[:space:]](.*)[[:space:]]+$ ]] ;then	a=${BASH_REMATCH[1]};fi
+ elif [[ $a =~ ^[[:space:]](.*)[[:space:]]+$ ]] ;then	a=.*${BASH_REMATCH[1]}.*
+ fi
+ D=;IFS=$'\n'
+ for u in `history|sed -nE "s/^\s+([0-9]+)\*?\s+$a$/\1/p"`
+ {
+  echo -e '\e[41;1;37m'`history $((B-u+1))|head -1`'\e[m'
+  ((D)) || b=$u
+  let ln[D++]=u
+ }
+ ! ((D)) &&{ echo -e "\e[41;1;37m$1\e[m wasn't found, did nothing";set --;continue 2;}
+ echo -en '\e[40;1;32m'
+ read -N1 -p "Delete $D line(s) above from command history? (Enter: yes Else: no) " h
+ [[ $h = $'\xa' ]] ||{ echo;set --;continue 2;}
+ unset IFS;for i	in `eval echo {$((D-1))..0}`
+ {
+  history -d ${ln[i]} 2>/dev/null
+ }
+ echo -e "Finished $D line(s) deletion\e[m";did=1
+fi
+B=$u
+[[ `history|head -1` =~ ^[[:space:]]*([0-9]+) ]]
+let B=HISTCMD-BASH_REMATCH[1]+1
+
 let C=HISTCMD-b
 ((u<b))&&{	m=$b;b=$u;u=$m; }
 ((b=b>4? b-4: 1))
