23856d31fedfbf46e288f759ce41d9aa83ab80a5
abdulbadii <abdulbadii@yandex.com>
2022-08-04 12:27:21 +0700

    fix

--- hist.sh
+++ hist.sh
@@ -1,10 +1,10 @@
 h(){ # BEGIN h
 [[ $1 =~ ^-cr|^-rc ]] &&{ history -c;history -r;return;}
 [[ $1 =~ ^--help$|^-[anprswdc]$|^\. ]] &&{ history ${@#.};return;}
-F=1;L=25;U=0;did=
+Z=1;L=25;U=0;did=
 while : ;do
 if [ -z "$1" ] ;then
-	((F))&&{	history 25;F=;	 }
+	((Z))&&{	history 25;Z=;	 }
  [[ `history|head -1` =~ ^[[:space:]]+([0-9]+) ]];((B=1+HISTCMD-(OF=BASH_REMATCH[1])))
 	IFS='';while : ;do
 		read -sN 1 -p "`echo $'\e[44;1;37m'`Next 25? Up/Down: from last/begin, [-]n[-][n] erase by range, Enter: out, Else: as substring `echo -e '\e[m\n\r'`" m
@@ -19,8 +19,8 @@ if [ -z "$1" ] ;then
      U=$L
 					if((((L+=25))>B)) ;then
 						let L-=B
-						history |head -$((25-L))
 						history $L
+						history |head $((L-25))
 					else
 						history $L| head -25
 					fi;;
@@ -28,7 +28,7 @@ if [ -z "$1" ] ;then
      L=$U
 					history $U	| head -25
 					((((U-=25))<0)) &&{
-						history| head -$((-U))
+						history| head $U
 						let U+=B ;}
 				esac
 			fi;;
@@ -36,63 +36,69 @@ if [ -z "$1" ] ;then
 		*)   			read -rei "$m" m; break
 		esac
 	done
- unset IFS b i j k l u Z D E n s t ln
 fi
-if [[ $m =~ ^(-?-?[0-9]+-?[0-9]*(\ +-?-?[0-9]+-?[0-9]*)*|-)(.*) ]];then
- for m in ${BASH_REMATCH[1]} ;{ n=$m\ $n ;}
+unset IFS b l u z D E n s t ln
+if [[ $m =~ ^(-?-?[0-9]+-?[0-9]*(\ +-?-?[0-9]+-?[0-9]*)*|^-)\ ?(.*) ]];then
+ for n in ${BASH_REMATCH[1]} ;{ s=$n\ $s ;}
  t=${BASH_REMATCH[3]}
+ [[ $t ]] &&echo also try search string \'$t\' 
 else t=$m
 fi
-t="${t//\\/\\\\}";t=${t//\\\\./\\.}
-set -- $n
-for n ;{
+set -- $s
+for n ;{ m=
  [[ $n != $1 ]] && echo -e '\e[40;1;32mThen:\e[m'
  if [[ $n =~ ^([0-9]+)(-([1-9][0-9]*)?)?$ ]];then
   let l=u=BASH_REMATCH[1]
-  ((l<OF)) &&{ echo $l is less than history start $OF, give it as $OF;l=$OF;}
+  ((l<OF)) &&{ echo $l is less than $OF, history starting number;l=$OF;}
   ((l=u=l?l:1));be='line was'
   [[ ${BASH_REMATCH[2]} ]] &&{
    u=${BASH_REMATCH[3]:-$HISTCMD}
    be="$((u-l+1)) lines were"
   }
+  ((l>HISTCMD)) || ((u>HISTCMD)) &&{ echo $u is beyond range;continue;}
   ((u<l)) &&{ t=$u;u=$l;l=$t; }
-  ((u>HISTCMD)) &&{ echo $u is beyond range;continue;}
   echo -en '\e[1;31m';history $((1+HISTCMD-l)) | head -$((u-l+1));echo -en '\e[m'
   for i in `eval echo {$u..$l}` ;{
-   history -d $i 2>/dev/null
-  };did=1;n=
+   history -d $i 2>/dev/null;}
+  did=1
  else  e=
   [[ $n =~ ^-([1-9][0-9]*)?$|^--([1-9][0-9]*)(-([1-9][0-9]*)?)?$ ]]
   if((d=BASH_REMATCH[2])) ;then
-   ((BASH_REMATCH[3])) && e=${BASH_REMATCH[4]:-1}
+   [[ ${BASH_REMATCH[3]} ]] && e=${BASH_REMATCH[4]:-1}
    ((l=1+HISTCMD-((D=d+U))))
    ((e>=d-1)) &&{ echo $e: invalid value, use a dash such -$d to delete line $l only;set --;continue;}
-   let s=d-e++ ;let E=e+U
-   be="$((D-E+1)) lines were"
+   be="$((s=d-e)) lines were"
+   let E=++e+U
   else
    ((d=${BASH_REMATCH[1]:-1}))
    ((l=1+HISTCMD-((E=D=d+U))))
    s=1;be='line was'
   fi
-  let u=1+HISTCMD-E
   ((d>25)) || ((D>B)) &&{ echo $d: out of list range;set --;continue;}
   echo -en '\e[1;31m'; history $d | head -$s ;echo -en '\e[m'
-  for i in `eval echo {$D..$E}`
-  {
+  for i in `eval echo {$D..$E}` ;{
    history -d -$i 2>/dev/null
-  };did=1;n="specified by $n"
+  };did=1
+  ((((u=-1-HISTCMD+E))==-l)) ||u=
+  m="\e[40;1;32m as specified by \e[41;1;37m$n";n=$l$u
  fi
  echo -e "\e[1;32mThe $be deleted\e[m"
- ((L=LO=1+HISTCMD-l+((lo=l>11? 11: l))))
- history $LO | head -$lo
- echo -e "  \e[1;32m   ...Here's where the found, deleted $be, line \e[41;1;37m$l-$u\e[m $n"
+ L=;let F=l-1
+ ((l>11)) &&{ let L=HISTCMD-l+12 ; F=11 ;}
+ ((l<12)) && history $((11-F))
+ history $L | head -$F
+ echo -e "  \e[1;32m   ...Here's where the found, deleted $be, line \e[41;1;37m$n$m\e[m"
  ((H=1+HISTCMD-l))
- ((U=H>11? 11: H));((H)) || { H=;U=5;}
- history $H |head -$U
- let U=H-U
+ ((U=H>11? 11: H))
+ ((H)) && history $H |head -$U
+ ((H<12)) &&{
+  let U=11-H
+  history |head -$U ;}
+ let U=HISTCMD-U
 }
-[[ $t ]] &&{ M=
- s=${t//\"/\\\"};s=${s//\//\\/};s=${s//\*/\\*};s=${s//.../.*}
+[[ $t ]] &&{
+ s=${t//\\/\\\\};s=${s//\\\\./\\.};s=${s//\"/\\\"}
+ s=${s//\//\\/};s=${s//\*/\\*};s=${s//.../.*}
  s=${s//\+/\\+};s=${s//\|/\\|};s=${s//\^/\\^};s=${s//\?/\\?};
  s=${s//\[/\\[};s=${s//\(/\\(};s=${s//\{/\\{}
  s=${s//\]/\\]};s=${s//\)/\\)};s=${s//\}/\\'}'}
@@ -108,29 +114,35 @@ for n ;{
  {
   [[ $h =~ ^[[:space:]]+([0-9]+)\*?[[:space:]]+$s ]]
   printf "\e[1;36m% 4d \e[m%s\e[41;1;37m%s\e[m%s\n" $((ln[Z]=BASH_REMATCH[1])) "${BASH_REMATCH[2]}" "${BASH_REMATCH[3]}" "${BASH_REMATCH[4]}"
-  ((Z++)) || l=$ln
+  ((z++)) || l=$ln
  }
- ! ((Z)) &&{ echo -e "\e[41;1;37m$t\e[m wasn't found, did nothing";set --;continue;}
+ ! ((z)) &&{ echo -e "\e[41;1;37m$t\e[m wasn't found, did nothing";set --;continue;}
  echo -en '\e[40;1;32m'
- ((u=-Z+((H=ln[--Z]))))
- if ((Z)) ;then
+ ((u=-z+((H=ln[--z]))))
+ if ((z)) ;then
   be='lines were';s=s
-  ((H-l>Z)) && M=", not consecutive between which the undeleted lines lie"
- else be='line was';s=;fi
+  ((H-l>z)) && M=", not consecutive between which the undeleted lines lie"
+ else be='line was';s=;M=
+ fi
  read -N1 -p "Delete $((Z+1)) line$s above from command history? (Enter: yes Else: no) " h
  [[ $h = $'\xa' ]] ||{ echo;set --;continue;}
- for ((i=Z; i>=0; i--)){
+ for ((i=z; i>=0; i--)){
   history -d ${ln[i]} 2>/dev/null ;};did=1
- echo -e "Finished, the $((Z+1)) $be deleted\e[m"
- ((L=LO=1+HISTCMD-l+((lo=l>11? 11: l))))
- history $LO | head -$lo
- echo -e "\e[40;1;32m   ...Here's where the found, deleted $be$M, as searched by string \e[41;1;37m$t\e[m"
+ echo -e "Finished, the $((z+1)) $be deleted\e[m"
+ L=;let F=l-1
+ ((l>11)) &&{ let L=HISTCMD-l+12 ; F=11 ;}
+ ((l<12)) && history $((11-F))
+ history $L | head -$F
+ echo -e "\e[40;1;32m   ...Here's where the found, deleted $be$M, as searched of string \e[41;1;37m$t\e[m"
  ((H=HISTCMD-u))
- ((U=H>11? 11: H));((H)) || { H=;U=5;}
- history $H |head -$U
- let U=H-U
+ ((U=H>11? 11: H))
+ ((H)) && history $H |head -$U
+ ((H<12)) &&{
+  let U=11-H
+  history |head -$U ;}
+ let U=HISTCMD-U
 }
-((F))&&break
+((Z))&&break
 set --
 done
 ((did)) &&{
