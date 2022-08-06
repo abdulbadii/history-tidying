903efd70e9ae50de0bd90bb34f930f6426df1a89
abdulbadii <abdulbadii@yandex.com>
2022-08-06 22:10:07 +0700

    fix

--- hist.sh
+++ hist.sh
@@ -7,7 +7,7 @@ if [ -z "$1" ] ;then
 	((G))&&{	history 25;G=;	 }
  [[ `history|head -1` =~ ^[[:space:]]+([0-9]+) ]];((B=1+HISTCMD-(OF=BASH_REMATCH[1])))
 	IFS='';while : ;do
-		read -sN 1 -p "`echo $'\e[44;1;37m'`Next 25? Up/Down: from last/begin, [-|--]n[=|-n] erase by range, Enter: out, Else: a search string `echo -e '\e[m\n\r'`" m
+		read -sN 1 -p "`echo $'\e[44;1;37m'`Next 25? Up/Down: to older/newer, [-|--]n[=|-n] erase by range, Enter: out, Else: a search string `echo -e '\e[m\n\r'`" m
 		case $m in
 		$'\x1b') # \e ESC
 			read -N 1 m
@@ -36,6 +36,7 @@ if [ -z "$1" ] ;then
 		*)   			read -rei "$m" m; break
 		esac
 	done
+else m=$*
 fi
 unset IFS l u z s
 if [[ $m =~ ^(([0-9]+(-[0-9]|=-?[0-9]*)?|-([1-9][0-9]*(=-?[0-9]*)?)?|--[1-9][0-9]*-?[0-9]*)+)(\ +(.*))?$ ]];then
@@ -45,8 +46,8 @@ if [[ $m =~ ^(([0-9]+(-[0-9]|=-?[0-9]*)?|-([1-9][0-9]*(=-?[0-9]*)?)?|--[1-9][0-9
 else Z=$m
 fi
 set -- $s
-for n ;{ unset ln e
- [[ $n != $1 ]] && echo -e '\e[40;1;32mThen:\e[m'
+for n ;{ unset ln e R
+ [[ $n != $1 ]] && echo -e '\e[40;1;32mThen...\e[m'
  if [[ $n =~ ^([0-9]+)((=-?|-)([0-9][0-9]*)?)?$ ]];then
   let l=BASH_REMATCH[1]
   ((l=l?l:1))
@@ -58,7 +59,7 @@ for n ;{ unset ln e
   }
   ((u<l)) &&{ T=$u;u=$l;l=$T; }
   ((l<OF)) || ((u>HISTCMD)) &&{
-   echo $n is beyond range, the history starting-end line number is $OF-$HISTCMD;continue;}
+   echo $n is beyond range, the history starting-end line numbers are $OF-$HISTCMD;continue;}
   n=$l-$u
   b="$((u-l+1)) lines were"
   ((l==u)) &&{
@@ -70,26 +71,25 @@ for n ;{ unset ln e
   [[ $n =~ ^-([1-9][0-9]*)?(=-?[0-9]*)?$|^--([1-9][0-9]*)(-([1-9][0-9]*)?)?$ ]]
   if((d=BASH_REMATCH[3])) ;then
    [[ ${BASH_REMATCH[4]} ]] && e=${BASH_REMATCH[5]:-1}
-   ((e>=d-1)) &&{ echo $e: invalid value, use a dash such -$d to delete line $l only;set --;continue;}
+   ((e>=d-1)) &&{ echo $e: invalid use, do such -$d to delete line $l only;set --;continue;}
    let ++e
   else
    d=${BASH_REMATCH[1]}
-   ((d=d?d:1))
-   t=${BASH_REMATCH[2]}
-   e=$d
-   [[ $t ]] &&{ t=${t#=}
+   ((e=d=d?d:1))
+   [[ ${BASH_REMATCH[2]} ]] &&{ t=${BASH_REMATCH[2]#=}
     ((${t#-})) ||t=${t}1
-    e=$d
-    let d-=t
+    ((R=(d-=t)<=0))
     ((d<e)) &&{ T=$d;d=$e;e=$T; }
    }
   fi  
+  ((R)) || ((d>50)) || ((D>B)) &&{ echo $d or $e: out of list range;set --;continue;}
   ((l=1+HISTCMD-((D=d+U))));let E=e+U
   ((((u=-l-d+e))==-l)) &&u=
   if ((s=d-e)) ;then b="$((++s)) lines were"
-  else b='line was';s=1 ;fi
-  ((d>25)) || ((D>B)) &&{ echo $d: out of list range;set --;continue;}
-  echo -en '\e[1;35m'; history $d | head -$s ;echo -en '\e[m'
+  else b='line was';s=1
+  fi
+  echo -en '\e[1;35m'; history $D | head -$s ;echo -en '\e[m'
+  history -w /tmp/.bash_history0
   for i in `eval echo {$D..$E}` ;{
    history -d -$i 2>/dev/null
   };did=1
@@ -101,7 +101,7 @@ for n ;{ unset ln e
  ((l>11)) &&{ let L=HISTCMD-l+12 ; F=11 ;}
  ((l<12)) && history $((11-F))
  history $L | head -$F
- echo -e "  \e[1;32m  ...Here's where the found, deleted $b, previously line \e[41;1;37m$n$M\e[m"
+ echo -e "  \e[1;32m Here's where the found, deleted $b, previously line \e[41;1;37m$n$M\e[m"
  let H=1+HISTCMD-l
  ((U=H>11? 11: H))
  ((H)) && history $H |head -$U
@@ -142,6 +142,7 @@ for n ;{ unset ln e
  fi
  read -N1 -p "Delete $((z+1)) line$s above from command history? (Enter: yes Else: no) " h
  [[ $h = $'\xa' ]] ||{ echo;set --;continue;}
+ history -w /tmp/.bash_history0
  for ((i=z; i>=0; i--)){
   history -d ${ln[i]} 2>/dev/null ;};did=1
  echo -e "Finished, the $((z+1)) $b deleted\e[m"
@@ -150,7 +151,7 @@ for n ;{ unset ln e
  ((l>11)) &&{ let L=HISTCMD-l+12 ; F=11 ;}
  ((l<12)) && history $((11-F))
  history $L | head -$F
- echo -e "\e[40;1;32m  ...Here's where the found, deleted $b$M, as of searched string \e[41;1;37m$Z\e[m"
+ echo -e "\e[40;1;32m Here's where the found, deleted $b$M, by search \e[41;1;37m$Z\e[m"
  let H=HISTCMD-u
  ((U=H>11? 11: H))
  ((H)) && history $H |head -$U
@@ -168,7 +169,9 @@ done
   IFS=$'\n';i=;for l in `history`
   {	[[ $l =~ ^[[:space:]]+([0-9]+)\*?[[:space:]]*$ ]] &&history -d $((BASH_REMATCH[1]-i++)); }
   history -w&&echo -n ..saved
-	elif	[[ ! $o =~ ^[nN]$ ]] ;then history -c;history -r;fi;echo -n $o
+	elif	[[ ! $o =~ ^[nN]$ ]] ;then
+  history -c;history -r /tmp/.bash_history0
+ fi;echo $o
 }
 unset IFS
 } #END h
